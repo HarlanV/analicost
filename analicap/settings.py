fe5d155427a075a9e97f8e15b8bbd58e07af87a7
HarlanV <harlan.eq@gmail.com>
2021-11-15 13:53:28 -0300

    testando docker

--- analicap/settings.py
+++ analicap/settings.py
@@ -96,6 +96,13 @@ DATABASES = {
         'PASSWORD': env("DATABASE_PASSWORD"),
         'HOST': env("DATABASE_HOST"),
         'PORT': env("DATABASE_PORT"),
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': env("DATABASE_NAME"),
+        # 'USER': env("DATABASE_USER"),
+        # 'PASSWORD': env("DATABASE_PASSWORD"),
+        # 'HOST': env("DATABASE_HOST"),
+        # 'PORT': env("DATABASE_PORT"),
     }
 }
 
